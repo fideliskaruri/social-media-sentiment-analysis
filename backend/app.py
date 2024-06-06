ed30f5ac7c1258f064d422879b67bd2bb7a5a904
Fidelis Karuri Wachira <itsfidelgray@gmail.com>
2024-06-06 07:56:22 +0000

    requirements.txt

--- backend/app.py
+++ backend/app.py
@@ -2,14 +2,35 @@ from flask import Flask, jsonify
 from YoutubeData import fetchAllComments
 from SentimentAnalysis import AnalyzeSentiment
 import pandas as pd
+from flask_cors import CORS
+import re
 import json
 
 app = Flask(__name__)
+CORS(app)
 
+import re
+
+def extract_video_id(youtube_url):
+    # Regular expression pattern to match YouTube video URLs
+    pattern = r"(?<=v=|\\/videos\\/|embed\\/|youtu.be\\/|\\/v\\/|\\/e\\/|watch\\?v=|&v=|%2Fvideos%2F|%2Fv%2F|%2Fe%2F|%2Fwatch%3Fv%3D|%26v%3D)([\\w-]+)"
+    
+    # Search for the video ID in the URL
+    match = re.search(pattern, youtube_url)
+    if match:
+        return match.group(1)
+    else:
+        return None
+
+
+@app.route('/')
+def Home():
+    return "Welcome, to analyze youtube comments just go to '/api/analyzecomments/<youtube_url>.\nExample: /api/analyzecomments/https://www.youtube.com/watch?v=iwGVLiFL-Lw' "
 
 @app.route('/api/analyzecomments/<video_id>')
 def analyze_youtube_comments(video_id):
 
+    # video_id = extract_video_id(youtube_url=youtube_url)
     comments = fetchAllComments(video_id)
     youtube_comments_analysis_results = AnalyzeSentiment(comments)
     
